92eaaac833f4e305020a116a0c95b0181ddbad0b
fidessolutions <george.kashirin@gmail.com>
2025-07-23 16:55:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    if (this.getAttribute('href').startsWith('#')) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
