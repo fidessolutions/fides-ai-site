75f3ef1c57f747dfe2d20fa9b40c5b02483b9eef
fidessolutions <george.kashirin@gmail.com>
2025-07-24 18:56:05 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', function(e) {
-    if (this.getAttribute('href').startsWith('#')) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      target.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
-});
